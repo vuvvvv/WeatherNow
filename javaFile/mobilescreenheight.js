170a177310a7fb92a634cb0fce78493b5117d2f7
vuvvvv <azooz51894@gmail.com>
2024-11-26 08:51:50 +0300

    2024-11-26

--- javaFile/mobilescreenheight.js
+++ javaFile/mobilescreenheight.js
@@ -1,37 +1,38 @@
-// دعم اجهزة الجوال الطويلة 
+// دعم اجهزة الجوال الطويلة
+//بس بشوف يتحدث في الجات هاب ولا ل
 
-function screenHeight(v){
-    v;
+function screenHeight (v) {
+  v
   switch (v) {
     case 532:
-      return document.querySelector('.openbtn').style.top = '25px'
+      return (document.querySelector('.openbtn').style.top = '25px')
     case 568:
-      return document.querySelector('.openbtn').style.top = '9px'
+      return (document.querySelector('.openbtn').style.top = '9px')
     case 667:
-      return  document.querySelector('.openbtn').style.top = '-20px'
+      return (document.querySelector('.openbtn').style.top = '-20px')
     case 640:
-      return document.querySelector('.openbtn').style.top = '-15px'
+      return (document.querySelector('.openbtn').style.top = '-15px')
     case 760:
-      return document.querySelector('.openbtn').style.top = '-50px'
+      return (document.querySelector('.openbtn').style.top = '-50px')
     case 820:
-      return document.querySelector('.openbtn').style.top = '-80px'
+      return (document.querySelector('.openbtn').style.top = '-80px')
     case 900:
-      return document.querySelector('.openbtn').style.top = '-115px'
+      return (document.querySelector('.openbtn').style.top = '-115px')
     case 915:
-      return document.querySelector('.openbtn').style.top = '-130px'
-    case 800  :
-      return document.querySelector('.openbtn').style.top = '-75px'
+      return (document.querySelector('.openbtn').style.top = '-130px')
+    case 800:
+      return (document.querySelector('.openbtn').style.top = '-75px')
     case 896:
-      return document.querySelector('.openbtn').style.top = '-130px'
+      return (document.querySelector('.openbtn').style.top = '-130px')
     case 844:
-      return document.querySelector('.openbtn').style.top = '-100px'
+      return (document.querySelector('.openbtn').style.top = '-100px')
     case 926:
-      return document.querySelector('.openbtn').style.top = '-130px'
+      return (document.querySelector('.openbtn').style.top = '-130px')
     case 846:
-      return document.querySelector('.openbtn').style.top = '-100px'
+      return (document.querySelector('.openbtn').style.top = '-100px')
     case 0:
     default:
-      return document.querySelector('.openbtn').style.top = ''
+      return (document.querySelector('.openbtn').style.top = '')
   }
 }
 screenHeight(screen.height)
